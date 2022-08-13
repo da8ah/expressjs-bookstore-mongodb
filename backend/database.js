d4e739ead4e3017004e26f916fb3b2b1ce89f76a
Tiber <danilo.ochoa.hidalgo@outlook.com>
2022-08-12 21:18:58 -0500

    Bookstore project with MongoDB Atlas

--- backend/database.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-import { MONGODB_URI } from "./config.js";
-
-try {
-  const db = await mongoose.connect(MONGODB_URI);
-  console.log("Database is connected to", db.connection.host);
-} catch (error) {
-  console.error(error);
-}
